6770ad4ebbb30aa23d4d37ea12beb8892e8fc2d4
Louvani <paula.louvani@gmail.com>
2021-07-20 19:46:17 -0500

    Task15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,12 +8,12 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (!tree)
+	if (!tree)
 		return (0);
-    if (!tree->left && !tree->right)
+	if (!tree->left && !tree->right)
 		return (1);
-    else if (tree->left && tree->right)
+	else if (tree->left && tree->right)
 		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-    else
-        return (0);
+	else
+		return (0);
 }
