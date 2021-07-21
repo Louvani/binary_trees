9f80d72dc7ef09d96439e1c97f4ad196b2c50d32
Louvani <paula.louvani@gmail.com>
2021-07-21 10:21:50 -0500

    Task 16 betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,24 +1,33 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_balance - This function
+* binary_tree_is_perfect - This function
 * measures the balance factor of a binary tree.
 * @tree: The binary tree to measure.
-* Return: 0 if tree is NULL.
+* Return: 0 if tree is not perfect, 1 if is perfect.
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int left, right;
+
 	if (!tree)
 		return (0);
 	if (binary_tree_balance(tree) == 0)
 	{
 		if (!tree->left && !tree->right)
 			return (1);
-		return (binary_tree_is_perfect(tree->left) * binary_tree_is_perfect(tree->right));
+			left = binary_tree_is_perfect(tree->left);
+			right = binary_tree_is_perfect(tree->right);
+		return (left * right);
 	}
 	return (0);
 }
-
+/**
+* binary_tree_balance - This function
+* measures the balance factor of a binary tree.
+* @tree: The binary tree to measure.
+* Return: 0 if tree is NULL.
+*/
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (!tree)
